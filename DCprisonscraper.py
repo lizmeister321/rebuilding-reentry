b0c48ed2fe8e5b975a596df6cf492569478fab5c
Lizzie Ellis <e.geiger.ellis@gmail.com>
2015-10-31 15:04:36 -0400

    fixed urls stuck on open

--- DCprisonscraper.py
+++ DCprisonscraper.py
@@ -1,5 +1,7 @@
 from datetime import date, timedelta, datetime
-#import urllib
+import urllib
+import urllib2
+
 #import slate 
 
 
@@ -13,30 +15,66 @@ from datetime import date, timedelta, datetime
 #		--Post files to web interface for download?
 
 
+
 # STEP 1: Access DC DOC website--url changes weekly
 # #### NOTE: Week in data runs Sat-Friday
-# #### ASSUME PULL RUNS ON A SUNDAY 
-data_url_prefix = 'doc.dc.gov/sites/default/files/dc/sites/doc/publication/attachements/'
-data_url_suffix = start_date + '%%20through' + end_date +'.pdf'
+# #### ASSUME PULL RUNS ON A SATURDAY 
+
+start_date= date.today()-timedelta(7)
+end_date=date.today()-timedelta(1)
+
+start_month=start_date.strftime('%B')
+start_day= start_date.strftime('%d')
+start_year=start_date.strftime('%Y')
+
+end_month=end_date.strftime('%B')
+end_day=end_date.strftime('%d')
+end_year=end_date.strftime('%Y')
+
+data_url_prefix = 'doc.dc.gov/sites/default/files/dc/sites/doc/publication/attachments/'
+
+
+
+	data_url_suffix = start_month + ' ' + start_day +  ' through ' + end_month + ' ' + end_day + ' ' + year+ '.pdf'
+	data_url_full= data_url_prefix + data_url_suffix	
+	clean_url=urllib.quote(data_url_full)
+
+clean_file='STATS_' + start_month + '-' + start_day + '_to_' + end_month + '-' + end_day + '-' + year
+
+
+##CURRENTLY STUCK HERE: 
+urllib.urlretrieve(urllib.quote(data_url_full),'test.pdf')
+##returns the following:
+
+# Traceback (most recent call last):
+#   File "/Users/Lellis/Desktop/PythonStuff/Rebuilding-Reentry/DCprisonscraper.py", line 41, in <module>
+#     urllib.urlretrieve(urllib.quote(data_url_full),'test.pdf')
+#   File "/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/urllib.py", line 98, in urlretrieve
+#     return opener.retrieve(url, filename, reporthook, data)
+#   File "/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/urllib.py", line 245, in retrieve
+#     fp = self.open(url, data)
+#   File "/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/urllib.py", line 213, in open
+#     return getattr(self, name)(url)
+#   File "/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/urllib.py", line 469, in open_file
+#     return self.open_local_file(url)
+#   File "/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/urllib.py", line 483, in open_local_file
+#     raise IOError(e.errno, e.strerror, e.filename)
+# IOError: [Errno 2] No such file or directory: 'doc.dc.gov/sites/default/files/dc/sites/doc/publication/attachments/October 24 through October 30 2015.pdf'
+# [Finished in 0.1s with exit code 1]
+
 
 #IF start_date_year = end+date_year, then start_date is month/day
 #ELSE start_date is month/day/year
 #end_date is always month/day/year 
 
-date.weekday() = 6
-
-end_date = 
-
-start_date = 
-	if 
-
-
-print date.today().strftime("%B" + '%%20' + '%d' +  '%%20through' + '%B' + '%%20' + '%d' + '%Y' + '.pdf') 		
-start_date=date.today().strftime('%B' + '%%20' + '')
 
 
 
-#Won't print %, will likely need to move to .format() method with dates as var
+
+
+
+
+
 
 
 # from cStringIO import StringIO
