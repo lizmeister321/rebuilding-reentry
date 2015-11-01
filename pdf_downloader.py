9cd4f1b7a61be9dd9a8a7dce771b30b679cd82f5
Valerie Woolard <valerie.woolard@gmail.com>
2015-11-01 12:01:53 -0500

    Try new parsing technique, clean up other stuff...

--- pdf_downloader.py
+++ pdf_downloader.py
@@ -1,6 +1,7 @@
-execfile('constants')
-
 import urllib2
+
+remote_urls = [line.rstrip('\n') for line in open('remote_urls')]
+
 for url in remote_urls:
      print "Downloading " + url
      file_name = url.split('/')[-1]
--- tabula_extractor.rb
+++ tabula_extractor.rb
@@ -1,17 +1,19 @@
-load 'constants'
+require 'tabula'
+
+file_names = []
+IO.readlines('file_names').each do |line|
+  file_names << line.chomp
+end
 
 file_names.each do |file_name|
     output_file_name = file_name.gsub('.pdf', '.csv')
     output_file = open("csv_files/#{output_file_name}", 'w')
-    extractor = Tabula::Extraction::ObjectExtractor.new("pdf_files/#{file_name}")
-    extractor.extract.each_with_index do |pdf_page, page_index|
+    puts "Extracting #{file_name} to #{output_file_name}"
+    extractor = Tabula::Extraction::ObjectExtractor.new("pdf_files/#{file_name}", :all)
+    extractor.extract.each do |pdf_page|
       # we should play with these settings to see if it gets us better results
-      page_areas = [[250, 0, 325, 1700]]
-      scale_factor = pdf_page.width / 1700
-      vertical_ruling_locations = [0, 360, 506, 617, 906, 1034, 1160, 1290, 1418, 1548] #column locations
-      vertical_rulings = vertical_ruling_locations.map{|n| Tabula::Ruling.new(0, n * scale_factor, 0, 1000)}
-      page_areas.each do |page_area|
-        output_file << pdf_page.get_area(page_area).get_table(:vertical_rulings => vertical_rulings).to_csv
+      pdf_page.spreadsheets.each do |spreadsheet|
+        output_file << spreadsheet.to_csv
         output_file << "\n\n"
       end
     end
